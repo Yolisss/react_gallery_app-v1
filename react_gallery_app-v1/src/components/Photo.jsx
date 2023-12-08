8ac5ccf365ab525789b156b824f6f82735792c1f
Yolisma <yolisisabel28@gmail.com>
2023-12-07 17:45:23 -0800

    initial set up and installed react router

---
+++ react_gallery_app-v1/src/components/Photo.jsx
@@ -0,0 +1,12 @@
+const Photo = () => {
+  return (
+    <div>
+      <h1>Photo Component</h1>
+      <li>
+        <img />
+      </li>
+    </div>
+  );
+};
+
+export default Photo;
---
+++ react_gallery_app-v1/src/components/PhotoList.jsx
@@ -0,0 +1,9 @@
+const PhotoList = () => {
+  return (
+    <div>
+      <h1>PhotoList Component</h1>
+    </div>
+  );
+};
+
+export default PhotoList;
